b6ee036ee8384ade7ad468c5fe9b1b5c0eb68108
Michael <michael.caitano@unionitdigital.com.br>
2021-10-26 17:58:44 -0300

    Created payment microservice

---
+++ mkd-payment/src/main/java/br/com/mkdelivery/payment/MkdPaymentApplication.java
@@ -0,0 +1,13 @@
+package br.com.mkdelivery.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkdPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkdPaymentApplication.class, args);
+	}
+
+}
