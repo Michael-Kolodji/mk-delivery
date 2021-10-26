dd351c1d8a771d5cd05dd6986dd1f8b514ed0c6f
Michael <michael.caitano@unionitdigital.com.br>
2021-10-26 16:49:32 -0300

    Finalization of customer microservice

--- mkd-client/src/main/java/br/com/mkdelivery/client/api/resources/ClientResource.java
+++ mkd-client/src/main/java/br/com/mkdelivery/client/api/resources/ClientResource.java
@@ -7,7 +7,7 @@ import javax.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -49,8 +50,11 @@ public class ClientResource {
 	
 	@GetMapping
 	@ResponseStatus(code = HttpStatus.OK)
-	public Page<ClientDTOResponse> findClients(ClientDTORequest dtoRequest, Pageable pageable) {
+	public Page<ClientDTOResponse> findClients(ClientDTORequest dtoRequest, 
+			@RequestParam("page") int pageIndex, @RequestParam("size") int pageSize) {
 		Client filter = mapper.map(dtoRequest, Client.class); 
+		
+		PageRequest pageable = PageRequest.of(pageIndex, pageSize);
 		
 		Page<Client> result = service.findClients(filter, pageable);
 		
