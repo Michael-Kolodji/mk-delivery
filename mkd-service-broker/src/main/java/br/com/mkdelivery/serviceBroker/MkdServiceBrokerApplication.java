5439dbb2fa1f6eaf29ce1a99022f118fce4647c9
Michael <michael.caitano@unionitdigital.com.br>
2021-12-23 16:31:29 -0300

    Create mkd-service-broker

---
+++ mkd-service-broker/src/main/java/br/com/mkdelivery/serviceBroker/MkdServiceBrokerApplication.java
@@ -0,0 +1,13 @@
+package br.com.mkdelivery.serviceBroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkdServiceBrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkdServiceBrokerApplication.class, args);
+	}
+
+}
---
+++ mkd-service-broker/src/test/java/br/com/mkdelivery/serviceBroker/MkdServiceBrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mkdelivery.serviceBroker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkdServiceBrokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
