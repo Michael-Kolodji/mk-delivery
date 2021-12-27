2db95b4bc2ef60f32a775c853f4fd52e295d63ec
Michael Kolodji <kolodjimichael@gmail.com>
2021-12-27 16:09:56 -0300

    Create Service Broker Consumer

---
+++ mkd-service-broker-consumer/src/main/java/br/com/mkdelivery/service/broker/consumer/MkdServiceBrokerConsumerApplication.java
@@ -0,0 +1,13 @@
+package br.com.mkdelivery.service.broker.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkdServiceBrokerConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkdServiceBrokerConsumerApplication.class, args);
+	}
+
+}
---
+++ mkd-service-broker-consumer/src/test/java/br/com/mkdelivery/service/broker/consumer/MkdServiceBrokerConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mkdelivery.service.broker.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkdServiceBrokerConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
