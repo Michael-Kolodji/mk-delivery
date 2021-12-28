926c4af54e850293cbfa133ae6b5480a149e26cc
Michael Kolodji <kolodjimichael@gmail.com>
2021-12-28 16:17:36 -0300

    mkd-process-payment created

---
+++ mkd-process-payment/src/main/java/br/com/mkdelivery/process/payment/MkdProcessPaymentApplication.java
@@ -0,0 +1,13 @@
+package br.com.mkdelivery.process.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkdProcessPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkdProcessPaymentApplication.class, args);
+	}
+
+}
---
+++ mkd-process-payment/src/test/java/br/com/mkdelivery/process/payment/MkdProcessPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mkdelivery.process.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkdProcessPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
